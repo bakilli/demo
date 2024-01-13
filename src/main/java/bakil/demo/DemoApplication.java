4c500e6faa9b538b8ef818c1dc9afa758f9b285a
bakilli <baris_kakilli38@hotmail.com>
2024-01-14 00:17:31 +0300

    Removed unnecessary code in DemoApplication.java

--- src/main/java/bakil/demo/DemoApplication.java
+++ src/main/java/bakil/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package bakil.demo;
 
-import bakil.demo.database.Flight;
-import bakil.demo.database.FlightRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,27 +7,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
-
-    private final FlightRepository repository;
-
-    public DemoApplication(FlightRepository repository) {
-        this.repository = repository;
-    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    public void run(String... args) throws Exception {
-        repository.deleteAll();
-
-        repository.save(new Flight("MIA", "LAX", "2021-01-01", "2021-01-02", "100"));
-        repository.save(new Flight("MIA", "LAX", "2021-01-01", "2021-01-02", "200"));
-
-        System.out.println("Flights found with findAll():");
-        System.out.println("-------------------------------");
-        for (Flight flight : repository.findAll()) {
-            System.out.println(flight);
-        }
     }
 
 }
